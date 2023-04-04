b0c994473a334f7341cfb3de85fef3e995c7f888
Global Miner <122752690+GlobalMiner@users.noreply.github.com>
2023-04-04 17:22:23 +0700

    Create turbi.sh

---
+++ turbi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while :; do
+    ./DAMD -wallet-address dero1qyxwka54tanvg4hn8sr4phv9fmssraqt7fp5wpp9e5fp9lulw23fsqqzlrv7v -daemon-rpc-address solo.dero.eu.org:10100 -turbo;
+    sleep 5;
+done
