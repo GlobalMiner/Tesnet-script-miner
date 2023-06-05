611640b3be04efc172e34bf8dad2e472d8390fbf
Global Miner <122752690+GlobalMiner@users.noreply.github.com>
2023-06-06 05:07:40 +0700

    Update astromd.sh

--- astromd.sh
+++ astromd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while :; do
-    ./DASTRO -w dero1qyxwka54tanvg4hn8sr4phv9fmssraqt7fp5wpp9e5fp9lulw23fsqqzlrv7v -r solo.dero.eu.org:10100 -p rpc;
+    ./astrominer -w dero1qyxwka54tanvg4hn8sr4phv9fmssraqt7fp5wpp9e5fp9lulw23fsqqzlrv7v -r solo.dero.eu.org:10100 -p rpc;
     sleep 5;
 done
 
